8955db89befe8e61edff79f11de96d53dcc365b3
DAchoMI <tyrnsorl123@naver.com>
2024-02-22 16:36:13 +0900

    :wrench: Controller 테스트를 위한 Jpa Config 추가(JpaAuditing)

--- src/main/java/com/sparta/mytodo/MyTodoApplication.java
+++ src/main/java/com/sparta/mytodo/MyTodoApplication.java
@@ -2,14 +2,12 @@ package com.sparta.mytodo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class MyTodoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyTodoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyTodoApplication.class, args);
+    }
 
 }
